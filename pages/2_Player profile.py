4890158df4d3ac46880afcc6c7b73751d2e54e8b
Gyetvai Marcell <78917870+corvinus-Z5KFWE@users.noreply.github.com>
2024-03-03 12:47:17 +0100

    upload files

---
+++ pages/2_Player profile.py
@@ -0,0 +1,134 @@
+#Streamlit page creator
+import streamlit as st
+import pandas as pd
+from mplsoccer import PyPizza
+import matplotlib.pyplot as plt
+
+
+df = pd.read_csv('Player_stats\player_stats_merged_with_full_names2.csv', sep = ';')
+params = [
+    "goals", "npxg", "xg_net", "pass_xa", "sca",
+    "touches_att_pen_area", "progressive_passes", "progressive_passes_received",
+    "dribbles_completed", "touches_live_ball", "tackles_won", "aerials_won_pct"
+]
+
+gkpercentiles = df[df["position"] == 'GK'][params].quantile(1)
+dpercentiles =  df[df["position"] == 'DF'][params].quantile(1)
+mpercentiles =  df[df["position"] == 'MF'][params].quantile(1)
+fpercentiles =  df[df["position"] == 'FW'][params].quantile(1)
+
+st.sidebar.header("Team")
+pos_team = st.sidebar.multiselect("Filter down to a team:", df["team"].unique())
+if pos_team:
+    df = df[df["team"].isin(pos_team)]  # Apply team filter directly
+
+st.sidebar.header("Player Position")
+position = st.sidebar.multiselect("Filter down to players with a specific position:", df["position"].unique())
+if position:
+    df = df[df["position"].isin(position)]  # Apply position filter directly
+
+player_position = df['position'].iloc[0]
+
+def calculate_playervalues(percentiles):
+    values = round((playervalues / percentiles * 100), 2).fillna(0)
+    values = values.iloc[0].tolist()
+    return values
+
+values = ['0', '0','0','0','0','0','0','0','0','0','0','0']
+
+st.sidebar.header("Player")
+player = st.sidebar.multiselect("Filter down to the player in action:", df["player"].unique())
+if player:
+    playerdf = df[df["player"].isin(player)]  #Loc the selected player
+    playervalues = playerdf[params]
+    if playerdf['position'].iloc[0] == 'GK':
+        values = calculate_playervalues(gkpercentiles)
+    elif playerdf['position'].iloc[0] == 'DF':
+        values = calculate_playervalues(dpercentiles)
+    elif playerdf['position'].iloc[0] == 'MD':
+        values = calculate_playervalues(mpercentiles)
+    else:
+        values = calculate_playervalues(fpercentiles)
+else: 
+    values = ['0', '0','0','0','0','0','0','0','0','0','0','0']
+
+# Original parameter names and their more expressive counterparts
+param_mapping = {
+    "goals": "Goals",
+    "npxg": "Non-Penalty\nExpected Goals",
+    "xg_net": "Goals minus\nExpected Goals",
+    "pass_xa": "Expected Assists",
+    "sca": "Shot-Creating\nActions",
+    "touches_att_pen_area": "Touches in\nattacking penalty area",
+    "progressive_passes": "Progressive Passes",
+    "progressive_passes_received": "Progressive Passes\nReceived",
+    "dribbles_completed": "Dribbles Completed",
+    "touches_live_ball": "Live-ball touches",
+    "tackles_won": "Tackles won",
+    "aerials_won_pct": "Percentage\nof aerials won"
+}
+
+# Transform the parameter names using the mapping
+attack_params = [param_mapping[param] for param in params]
+
+
+#if match_id or pos_team or player:
+if player:
+# instantiate PyPizza class
+    baker = PyPizza(
+        params=attack_params,                  # list of parameters
+        straight_line_color="#F2F2F2",  # color for straight lines
+        straight_line_lw=1,             # linewidth for straight lines
+        last_circle_lw=0,               # linewidth of last circle
+        other_circle_lw=0,              # linewidth for other circles
+    )
+
+    plotcolor = playerdf['team_color'].iloc[0]
+    player = playerdf['player'].iloc[0]
+    pos = playerdf['position'].iloc[0]
+# plot pizza
+    fig, ax = baker.make_pizza(
+        values,                                      # list of values
+        figsize=(8, 8),                              # adjust figsize according to your need
+        color_blank_space=["#C5C5C5"]*len(params),   # use same color to fill blank space
+        blank_alpha=0.4,                             # alpha for blank-space colors
+        kwargs_slices=dict(
+            facecolor= plotcolor, edgecolor="#F2F2F2",
+            zorder=2, linewidth=3
+        ),                                           # values to be used when plotting slices
+        kwargs_params=dict(
+            color="#000000", fontsize=12,
+            va="center"
+        ),                                           # values to be used when adding parameter
+        kwargs_values=dict(
+            color="#000000", fontsize=10,zorder=100,
+            bbox=dict(
+                edgecolor="#000000", facecolor= "#C5C5C5",
+                boxstyle="round,pad=0.4", lw= 1
+            )
+        )                                            # values to be used when adding parameter-values
+    )
+    # add title
+    fig.text(
+        0.515, 0.97, f"{player}'s stats", size=18,
+        ha="center", color="#000000"
+    )
+    # add subtitle
+    fig.text(
+        0.515, 0.942,
+        f"Rank vs Other {pos}'s | 2022 World Cup",
+        size=15,
+        ha="center", color="#000000"
+    )
+    # add credits
+    CREDIT_1 = "data: statsbomb viz fbref"
+    fig.text(
+        0.99, 0.005, f"{CREDIT_1}", size=9,
+        color="#000000",
+        ha="right"
+    )
+    st.pyplot(fig)
+    plt.tight_layout()  
+else: 
+    st.write("No data available for the selected filters.")
+
