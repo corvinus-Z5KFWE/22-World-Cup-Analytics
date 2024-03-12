02cdc3bc2a2094cb6dfb5b3e3eb31e570d0e7694
Gyetvai Marcell <78917870+corvinus-Z5KFWE@users.noreply.github.com>
2024-03-12 18:15:27 +0100

    check

--- pages/2_Player profile.py
+++ pages/2_Player profile.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 
 
 df = pd.read_csv('player_stats_merged_with_full_names2.csv', sep = ';')
+duplicate_indexes = df.index[df.index.duplicated()]
+print("Duplicate Indexes:", duplicate_indexes)
 params = [
     "goals", "npxg", "xg_net", "pass_xa", "sca",
     "touches_att_pen_area", "progressive_passes", "progressive_passes_received",
